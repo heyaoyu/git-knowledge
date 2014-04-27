d02c8e351d1e63522e771dff0f82f8d4755e7c3a
heyaoyu <heyaoyu@gmail.com>
2014-04-27 20:48:36 +0800

    local change again

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 def main():
   print 'local change'
   print 'main'
-  print 'another change will conflict'
+  print 'another change will conflict, both added'
+  print 'local change will conflict, both added'
 
 if __name__=='__main__':
   main()
